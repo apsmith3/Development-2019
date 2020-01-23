1bc2936c77355e54716cc471b2128c0fecc0feda
apsmith3 <32570565+apsmith3@users.noreply.github.com>
2020-01-23 12:22:55 +0000

    added comments

--- Prototype/Light Control.js
+++ Prototype/Light Control.js
@@ -7,202 +7,220 @@ var input = [];
 var sequence = [];
 var correct = true;
 var roundcomplete = true;
-
+var sequencetotal;
+                    // Sets difficulty on button click
+                    // easy
 const easy = document.getElementById('Easy');
 easy.addEventListener("click", seteasy);
 
-function seteasy() {
+function seteasy()
+    {
 
-    iseasy = true;
-    ismedium = false;
-    ishard = false;
-    isendless = false;
-    console.log("easy");
-};
-
+        iseasy = true;
+        ismedium = false;
+        ishard = false;
+        isendless = false;
+        console.log("easy");
+    }
+                    // medium
 const medium = document.getElementById('Medium');
 medium.addEventListener("click", setmedium);
 
-function setmedium() {
+function setmedium()
+    {
 
-    iseasy = false;
-    ismedium = true;
-    ishard = false;
-    isendless = false;
-    console.log("medium");
-}
-
+        iseasy = false;
+        ismedium = true;
+        ishard = false;
+        isendless = false;
+        console.log("medium");
+    }
+                    // hard
 const hard = document.getElementById('Hard');
 hard.addEventListener("click", sethard);
 
-function sethard() {
+function sethard()
+    {
 
-    iseasy = false;
-    ismedium = false;
-    ishard = true;
-    isendless = false;
-    console.log("hard");
-}
-
+        iseasy = false;
+        ismedium = false;
+        ishard = true;
+        isendless = false;
+        console.log("hard");
+    }
+                    // endless
 const endless = document.getElementById('Endless');
 endless.addEventListener("click", setendless)
 
-function setendless() {
+function setendless()
+    {
 
-    iseasy = false;
-    ismedium = false;
-    ishard = false;
-    isendless = true;
-    console.log("endless");
-}
+        iseasy = false;
+        ismedium = false;
+        ishard = false;
+        isendless = true;
+        console.log("endless");
+    }
+                    // user inputs for each light
+                    // red/1
 const red = document.getElementById('red');
 red.addEventListener("click", add1);
-function add1() {
-    input.push(1);
-    checkinput();
-}
+function add1()
+    {
+        input.push(1);
+        checkinput();
+    }
+                    //orange/2
 const orange = document.getElementById('orange');
 orange.addEventListener("click", add2);
-function add2() {
-    input.push(2);
-    checkinput();
-}
+function add2()
+    {
+        input.push(2);
+        checkinput();
+    }
+                    //purple/3
 const purple = document.getElementById('purple');
 purple.addEventListener("click", add3);
-function add3() {
-    input.push(3);
-    checkinput();
-}
+function add3()
+    {
+        input.push(3);
+        checkinput();
+    }
+                    //blue/4
 const blue = document.getElementById('blue');
 blue.addEventListener("click", add4);
-function add4() {
-    input.push(4);
-    checkinput();
-}
+function add4()
+    {
+        input.push(4);
+        checkinput();
+    }
+                    //green/5
 const green = document.getElementById('green');
 green.addEventListener("click", add5);
-function add5() {
-    input.push(5);
-    checkinput();
-}
+function add5()
+    {
+        input.push(5);
+        checkinput();
+    }
+                    //yellow/6
 const yellow = document.getElementById('yellow');
 yellow.addEventListener("click", add6);
-function add6() {
-    input.push(6);
-    checkinput();
-}
+function add6()
+    {
+        input.push(6);
+        checkinput();
+    }
+                    // functions sets memory sequence length based on difficulty chosen
+function getdifficulty(sequencetotal)
+    {
+        if(iseasy == true){
+            sequencetotal = 7
+        }
+        if(ismedium == true){
+            sequencetotal = 13
 
-function getdifficulty(sequencetotal){
-    if(iseasy == true){
-        sequencetotal = 6
+        }
+        if(ishard == true){
+            sequencetotal = 19
+        }
+        if(isendless == true){
+            sequencetotal = 128
+        }
+        return sequencetotal
     }
-    if(ismedium == true){
-        sequencetotal = 12
-
-    }
-    if(ishard == true){
-        sequencetotal = 18
-    }
-    if(isendless == true){
-        sequencetotal = 128
-    }
-    return sequencetotal
-}
-
+                    // starts game on click resets game parameters and starts round
 const game = document.getElementById('StartGame');
 game.addEventListener("click", startgame);
 function startgame()
-{
-    input = [];
-    sequence = [];
-    roundcomplete = true;
-    correct = true;
-    startround();
+    {
+        input = [];
+        sequence = [];
+        roundcomplete = true;
+        correct = true;
+        sequencetotal = getdifficulty(); // total light sequence length based on difficulty
+        startround();
 
-}
-
+    }
+                    // a round
 function startround()
-{
-    var sequencetotal;
-
-    sequencetotal = getdifficulty();
-
-    while ((sequence.length < sequencetotal) && (correct == true) && (roundcomplete == true))
     {
-        roundcomplete = false;
-        sequence = sequencegenerator(sequence);
-        // CONTROLL LIGHTS
-        checkinput(sequence, input);
-    }
-    if (correct == false)
-    {
-        // set lights red
-        alert("Incorrect GAME OVER")
-    }
-    if ((correct == true) && (sequencetotal == sequence.length))
-    {
-        // set lights green
-        alert("You Win!!!")
-    }
 
-}
 
+
+        while ((sequence.length <= sequencetotal) && (correct == true) && (roundcomplete == true))  // runs round while previous round is completed correctly and light sequence is less than total for game
+        {
+            roundcomplete = false;
+            sequence = sequencegenerator(sequence);    // calls function to generate next sequence
+            // CONTROLL LIGHTS
+            checkinput(sequence, input);               // calls function to check user input
+        }
+        if (correct == false)                           // if correct is returned false
+        {
+            // set lights red
+            alert("Incorrect GAME OVER")
+        }
+        if ((correct == true) && (sequencetotal == sequence.length))
+        {
+            // set lights green
+            alert("You Win!!!")
+        }
+
+    }
+                    // randomly generates next light to be turned on and adds it to light sequence
 function sequencegenerator()
-{
-        var nextnum = Math.floor(Math.random() * 6) + 1;
-        sequence.push(nextnum);
-        console.log(sequence);
-return sequence;
-}
-
-
-
-
-
-
-function checkinput()
-{
-
-if(input.length == sequence.length)
     {
-        var z = true;
-        var y = 0;
-        var x = sequence.length;
-       while((y != x) && (z == true))
-       {
-           if((sequence[y] == input[y]) && (z == true))
-           {
-            y ++;
-           }
-           else
-           {
-            z = false;
-           }
-
-
-       }
-
-
-
-        if (z == true)
-        {
-            console.log("correct");
+            var nextnum = Math.floor(Math.random() * 6) + 1;
+            sequence.push(nextnum);
             console.log(sequence);
-            console.log(input);
-            input = [];
-            correct = true;
-            roundcomplete = true;
-            startround()
-        }
+            return sequence;
+    }
 
-        if (z == false)
+
+
+
+
+                    // checks user input
+function checkinput()
+    {
+
+    if(input.length == sequence.length) // once input length has reached light sequence lenth
         {
-            console.log("incorrect");
-            correct = false;
-            startround()
+            var z = true;
+            var y = 0;
+            var x = sequence.length;
+           while((y != x) && (z == true))           // checks if both input and light sequence arrays are the same
+            {
+               if((sequence[y] == input[y]) && (z == true))
+               {
+                    y ++;
+               }
+               else
+               {
+                    z = false;
+               }
+
+
+            }
+
+            if (z == true)                  // if correct returns correct parameters and starts next round
+            {
+                                    //console.log("correct");
+                                    //console.log(sequence);
+                                    //console.log(input);
+                input = [];
+                correct = true;
+                roundcomplete = true;
+                startround()
+            }
+
+            if (z == false)             // returns incorrect parameters
+            {
+                                    //console.log("incorrect");
+                correct = false;
+                startround()
+            }
         }
     }
-}
 
 
 
@@ -211,7 +229,7 @@ if(input.length == sequence.length)
 
 
 
-function getLightURI(element)
+function getLightURI(element)           // sets up light url to send to bridge
 {
     var IP = "http://192.168.0.50/api/";
     var username = "stlaB2I6VZ8O80Qepc-1xfmLrHgyTFvB9IGupaQz";
@@ -220,26 +238,3 @@ function getLightURI(element)
     return URI + element.attr("id")+"/";
 }
 
-function activatelight(element)
-{
-    var getState = $.getJSON(getLightURI(element), function (data)
-    {
-        var state = data["state"]["on"];
-        if (state)
-        {
-            state = false;
-        }
-        else
-        {
-            state = true;
-        }
-
-        var lightState = {"on" : state};
-
-        $.ajax({
-            url: getLightURI(element) + "state/",
-            type: "PUT",
-            data: JSON.stringify(lightState)
-        })
-    });
-}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Development-2019
+
+About Application:
+This Application is a memory game designed to make use of six Philips hue 
+lights as set up in SMB109. This application will flash the lights in a randomly 
+generated sequence and then prompt the user to input the same sequence back into the site 
+and determine whether they recalled the sequence correctly or not.
+
+It has four difficulty options easy, medium, hard, endless, to be selected by the user.
+
+Each light is assigned its own colour to remain consistent during the game.
+
+These lights flash in a randomly generated order starting with just one flash then waiting for the user to indicate which of the lights using input buttons on the application page laid out and colour to represent each of the lights.
+
+The application will then randomly add another light to the sequence of flashes and again wait for the user to input the corresponding sequence in the correct order.
+
+This will repeat until the sequence length required to complete the game has been reached based on the difficulty previously chosen.
+
+If the user incorrectly inputs the pattern the lights will flash red along with an alert indicating that they were incorrect, and the game will stop.
+
+If the user correctly reaches the end of the game, all the lights will flash green and an alert will pop up indicating success.
